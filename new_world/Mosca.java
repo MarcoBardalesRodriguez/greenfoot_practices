69acac4ae2835b86f8864b0b1c4c77e9855cc0d9
MarcoBardalesRodriguez <marcoantoniobardalesrodriguez@gmail.com>
2022-09-07 18:26:22 -0500

    -

--- new_world/Mosca.java
+++ new_world/Mosca.java
@@ -4,20 +4,53 @@ public class Mosca extends Actor
 {
     public void act()
     {
-        int y = getY();
-        
-        if (Greenfoot.isKeyDown("e")){
-            move(10);
+        Actor Mosca;
+        Mosca = getOneObjectAtOffset(0, 0, Mosca.class);
+
+        // World mundo;
+        // mundo = getWorld();
+
+        // if (Mosca!=null){
+        // mundo.removeObject(Mosca);
+        // }
+        while (Mosca==Mosca) {
+            int random = Greenfoot.getRandomNumber(8);
+            int y = getY();
+
+            switch(random) {
+                case 0:
+                    // right
+                    move(10);
+                case 1:
+                    // left
+                    move(-10);
+                case 2:
+                    // top
+                    y-=10;
+                case 3:
+                    // bottom
+                    y+=10;
+                case 4:
+                    // top rigth
+                    y-=10;
+                    move(10);
+                case 5:
+                    // bottom right
+                    y+=10;
+                    move(10);
+                case 6:
+                    // top left
+                    y-=10;
+                    move(-10);
+                case 7:
+                    // bottom left
+                    y+=10;
+                    move(-10);
+                default:
+                    // code block
+            }
+            setLocation(getX(),y);
         }
-        if (Greenfoot.isKeyDown("a")){
-            move(-10);
-        }
-        if (Greenfoot.isKeyDown(",")){
-            y-=10;
-        }
-        if (Greenfoot.isKeyDown("o")){
-            y+=10; 
-        }
-        setLocation(getX(),y);
+
     }
 }
